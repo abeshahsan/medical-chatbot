354697ce75a3bd4be15cbd5079fec0abf38e09fe
abeshahsan <abeshahsan2002@gmail.com>
2026-03-19 02:12:42 +0600

    Add VSCode settings and update author email in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="medical-chatbot",
-    author="Abesh Ahsan",
     version="0.1.0",
+    author="Abesh Ahsan",
+    author_email="abeshahsan2002@gmail.com",
     packages=find_packages(),
 )
